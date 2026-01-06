051aac0e5609d4c395358ff7692b1c7f68f90a48
PashaahsaP <ppsairda@yandex.ru>
2026-01-06 14:38:20 +0700

    set constructor field in vm

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -8,37 +8,69 @@ using System.Windows.Input;
 
 namespace barcode_gen
 {
+    #region Helper class
+    /// <summary>
+    /// Cоотношение высоты и ширины
+    /// </summary>
+    public sealed class AspectRatio
+    {
+        public int Width { get; }
+        public int Height { get; }
 
+        public AspectRatio(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Value => (double)Width / Height;
+    }
+    /// <summary>
+    /// Отдает соотношение сторон наклеек
+    /// </summary>
+    /// 
+    public static class StickerRatios
+    {
+        public static readonly AspectRatio Small = new AspectRatio(3, 2);
+        public static readonly AspectRatio Large = new AspectRatio(10, 7);
+    }
+    #endregion
 
     internal class MainViewModel : INotifyPropertyChanged
     {
-
+        #region varible
         private double _widthConstructorContainer = 800 ;
         private double _heightConstructorContainer = 450 ;
         private double _widthBorder;
         private double _heightBorder;
         private double _widthCanvas;
         private double _heightCanvas;
-
-        private void AddBlock()
-        {
-            Blocks.Add(new BlockViewModel(RemoveBlock));
-        }
-        private void RemoveBlock(BlockViewModel block)
-        {
-            Blocks.Remove(block);
-        }
+        #endregion
+        #region properties
         private Mode _selectedMode = Mode.Large;
         public ObservableCollection<BlockViewModel> Blocks { get; }
                             = new ObservableCollection<BlockViewModel>();
-        public ICommand AddBlockCommand { get; }
         public Mode SelectedMode
         {
             get => _selectedMode;
             set
             {
                 _selectedMode = value;
-                OnPropertyChanged();
+                AspectRatio ar = null;
+                switch (value)
+                {
+                    case Mode.Large: ar =  new AspectRatio( StickerRatios.Large.Width, StickerRatios.Large.Height); break;
+                    case Mode.Small: ar = new AspectRatio( StickerRatios.Small.Width, StickerRatios.Small.Height); break;
+                    default: ar = new AspectRatio(StickerRatios.Small.Width, StickerRatios.Small.Height); break;
+                };
+
+
+
+               // HeightBorder = (WidthBorder / ar.Width) * ar.Height;
+               // HeightCanvas = (WidthCanvas / ar.Width) * ar.Height; 
+                OnPropertyChanged(nameof(SelectedMode));
+                OnPropertyChanged(nameof(HeightBorder));
+                OnPropertyChanged(nameof(HeightCanvas));
             }
         }
         public double WidthConstructorContainer
@@ -47,7 +79,7 @@ namespace barcode_gen
             set
             {
                 _widthConstructorContainer = value;
-                _widthBorder  = value / 3 - 45;
+                WidthBorder  = value / 3 - 45;
                 OnPropertyChanged(nameof(WidthConstructorContainer));
                 OnPropertyChanged(nameof(WidthBorder));
             }
@@ -58,7 +90,7 @@ namespace barcode_gen
             set
             {
                 _heightConstructorContainer = value;
-                _heightBorder = value / 3;
+                HeightBorder = value / 3;
                 OnPropertyChanged(nameof(HeightConstructorContainer));
                 OnPropertyChanged(nameof(HeightBorder));
 
@@ -69,10 +101,9 @@ namespace barcode_gen
             get => _widthBorder; set 
             {
                 _widthBorder = value;
-                _widthCanvas = value;
-                OnPropertyChanged(nameof(WidthCanvas));
+                WidthCanvas = value;
                 OnPropertyChanged(nameof(WidthBorder));
-
+                OnPropertyChanged(nameof(WidthCanvas));
             }
 
 
@@ -81,30 +112,55 @@ namespace barcode_gen
         {
             get => _heightBorder; set {
                 _heightBorder = value;
-                _heightCanvas = value;
-                OnPropertyChanged(nameof(HeightCanvas));
+                HeightCanvas = value;
                 OnPropertyChanged(nameof(HeightBorder));
-
+                OnPropertyChanged(nameof(HeightCanvas));
             }
 
         }
         public double WidthCanvas
         {
-            get => _widthBorder - 6; set => _widthCanvas = value;
+            get => _widthBorder - 6; set 
+            {
+                _widthCanvas = value; 
+                OnPropertyChanged(nameof(WidthCanvas));
+            }
 
         }
         public double HeightCanvas
         {
-            get => _heightBorder - 6; set => _heightCanvas = value;
+            get => _heightBorder - 6; set 
+            { 
+                _heightCanvas = value; 
+                OnPropertyChanged(nameof(HeightCanvas)); 
+            }
 
         }
+        #endregion
+        #region command
+        public ICommand AddBlockCommand { get; }
+        #endregion
+        #region ctor
         public MainViewModel()
         {
             AddBlockCommand = new RelayCommand(AddBlock);
         }
+        #endregion
+        #region methods
+        private void AddBlock()
+        {
+            Blocks.Add(new BlockViewModel(RemoveBlock));
+        }
+        private void RemoveBlock(BlockViewModel block)
+        {
+            Blocks.Remove(block);
+        }
+        #region onProperty
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(
             [CallerMemberName] string name = null)
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        #endregion
+        #endregion
     }
 }
